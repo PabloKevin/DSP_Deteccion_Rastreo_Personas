7e7bddf7fe64c09315ddd0750b7596f24c6fd19a
PabloKevin <pablo.pereira.p@estudiantes.utec.edu.uy>
2025-11-18 02:18:30 -0300

    Finalizado deteccion con OpenCV

--- captura_video.py
+++ captura_video.py
@@ -5,12 +5,14 @@ import queue
 from collections import deque
 import numpy as np
 
+
 class captura_video:
     def __init__(self, fps=60.0, camera=cv2.VideoCapture(0), video_path="DSP_Deteccion_Rastreo_Personas/videos/"):
         self.fps = fps
         self.cam = camera
-        self.frame_width = int(self.cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.frame_height = int(self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.frame_width = int(self.cam.get(cv2.CAP_PROP_FRAME_WIDTH)) # 640 px
+        self.frame_height = int(self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT)) # 480 px
+        print(f"Resolución de la cámara: {self.frame_width}x{self.frame_height}")
         # Define the codec and create VideoWriter object
         self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         self.video_path = video_path
@@ -19,6 +21,7 @@ class captura_video:
         self.video_queue = queue.Queue()
 
     def record(self, save_video=False, pipe_filtrado=None):
+        """Inicia la captura de video desde la cámara, mostrando en pantalla y opcionalmente guardando en disco."""
         # Iniciar los hilos
         reader_thread = threading.Thread(target=self.camera_reader, args=(save_video,pipe_filtrado,))
         writer_thread = threading.Thread(target=self.video_writer)
@@ -41,7 +44,7 @@ class captura_video:
                             (10, 30),                         # Posición (x, y)
                             cv2.FONT_HERSHEY_SIMPLEX,         # Tipo de fuente
                             1,                                # Escala de la fuente
-                            (255, 255, 255),                      # Color (BGR: Verde)
+                            (255, 255, 255),                      # Color (BGR: Blanco)
                             2)                                # Grosor del texto
                 
                 cv2.imshow('Camera', frame)
@@ -72,8 +75,6 @@ class captura_video:
             writer_thread.join()
         print("Listo.")
 
-        
-
 
     def camera_reader(self, save_video, pipe_filtrado):
         """Lee fotogramas de la cámara tan rápido como sea posible."""
@@ -83,6 +84,8 @@ class captura_video:
         while True:
             start = time.time()
             ret, frame = self.cam.read()
+            #end = time.time()
+            #print("Tiempo en obtener frame:", end-start) #0.053 segundos aprox, lo cual es un cuello de botella desde raíz.
             if not ret:
                 break
             
@@ -122,7 +125,7 @@ class captura_video:
         print("Finalizando hilo de escritura.")
         out.release()
 
-def hex2bgr(hex_color):
+def hex2bgr(hex_color): 
     """Convierte una cadena HEX (#RRGGBB) a una tupla BGR para OpenCV."""
     # Eliminar el '#' si está presente
     hex_color = hex_color.lstrip('#')
@@ -133,83 +136,94 @@ def hex2bgr(hex_color):
     # Reordena a BGR para OpenCV
     bgr = (rgb[2], rgb[1], rgb[0])
     return bgr
-    
-def pipeEdges(frame):
-    """Pipeline de procesamiento de fotogramas."""
-    start = time.time()
-    # Convierte a escala de grises
-    processed_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Aplica un desenfoque gaussiano
-    #processed_frame = cv2.GaussianBlur(processed_frame, (3, 3), 0)
-    #processed_frame = cv2.blur(processed_frame, (3, 3))
-    #processed_frame = cv2.medianBlur(processed_frame, 3)
-    processed_frame = cv2.bilateralFilter(processed_frame, 5, 30, 50) # Busca reducir ruido cuidando mantener bordes. Calibrado con Canny
-    #processed_frame = cv2.bilateralFilter(processed_frame, 13, 75, 75) # Calibrado con Sobel
-    # Aplica método de detección de bordes
-    processed_frame = cv2.Canny(processed_frame, 35, 60)
-    #sobelx = cv2.Sobel(processed_frame, cv2.CV_64F, 1, 0, ksize=13)
-    #sobely = cv2.Sobel(processed_frame, cv2.CV_64F, 0, 1, ksize=13)
-    #processed_frame = cv2.magnitude(sobelx*0.45, sobely*0.45)
-    end = time.time()
-    print(f"Tiempo de procesamiento del frame: {(end - start):.2} segundos")
-    return processed_frame
 
-def pipeFacesCV2(frame):
-    """Pipeline de procesamiento de fotogramas para detección de rostros usando OpenCV."""
-    start = time.time()
+class Pipelines_ImageProcessing:
+    def __init__(self):
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml') #haarcascade_frontalface_default
+        self.profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+        self.eyes_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+        self.smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bilateralFilter(gray, 5, 30, 50)
-    
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml') #haarcascade_frontalface_default
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+    def edges(self, frame):
+        """Pipeline de procesamiento de fotogramas para suvizado y detección de bordes."""
+        start = time.time()
+        # Convierte a escala de grises
+        processed_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Aplica un desenfoque gaussiano
+        #processed_frame = cv2.GaussianBlur(processed_frame, (3, 3), 0)
+        #processed_frame = cv2.blur(processed_frame, (3, 3))
+        #processed_frame = cv2.medianBlur(processed_frame, 3)
+        processed_frame = cv2.bilateralFilter(processed_frame, 5, 30, 50) # Busca reducir ruido cuidando mantener bordes. Calibrado con Canny
+        #processed_frame = cv2.bilateralFilter(processed_frame, 13, 75, 75) # Calibrado con Sobel
+        # Aplica método de detección de bordes
+        processed_frame = cv2.Canny(processed_frame, 35, 60)
+        #sobelx = cv2.Sobel(processed_frame, cv2.CV_64F, 1, 0, ksize=13)
+        #sobely = cv2.Sobel(processed_frame, cv2.CV_64F, 0, 1, ksize=13)
+        #processed_frame = cv2.magnitude(sobelx*0.45, sobely*0.45)
+        end = time.time()
+        #print(f"Tiempo de procesamiento del frame: {(end - start):.2} segundos")
+        return processed_frame
 
-    profile = False
-    if len(faces) == 0:
-        profile = True
-        profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
-        faces = profile_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
-    
-    eyes_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-    eyes = eyes_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4, minSize=(30, 30)) 
+    def faceComponentsCV2(self, frame):
+        """Pipeline de procesamiento de fotogramas para detección de cara o perfil, ojos y boca usando OpenCV."""
+        start = time.time()
 
-    smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
-    smile = smile_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=7, minSize=(30, 20), maxSize=(80,40)) 
-    
-    face_x, face_y, face_w, face_h = 0,0,0,0
-    for (x, y, w, h) in faces:
-        face_x, face_y, face_w, face_h = x, y, w, h
-        if profile:
-            color = "#650EE8"
-            cv2.rectangle(frame, (x, y), (x + w, y + h), hex2bgr(color), 2)
-            cv2.putText(frame, 'profile', (x, y-10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.9, hex2bgr(color), 1)
-        else:
-            color = "#4E75E8"
-            cv2.rectangle(frame, (x, y), (x + w, y + h), hex2bgr(color), 2)
-            cv2.putText(frame, 'face', (x, y-10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.9, hex2bgr(color), 1)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.bilateralFilter(gray, 5, 30, 50)
+        
+        faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
 
-    for (x, y, w, h) in eyes:
-        if y > face_y and y < face_y+face_h*2/3 and x > face_x and x < face_x+face_w:
-            color = "#C0E80E"
-            cv2.putText(frame, 'eye', (x, y-10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.9, hex2bgr(color), 1)
-            x = x + w//2
-            y = y + h//2
-            cv2.circle(frame, (x, y), w//2, hex2bgr(color), 2)
-            
+        profile = False
+        if len(faces) == 0:
+            profile = True
+            faces = self.profile_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+        
+        eyes = self.eyes_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4, minSize=(30, 30)) 
 
-    for (x, y, w, h) in smile:
-        # Asegurar que la sonrisa esté dentro de la región segura de la cara y filtrar falsas detecciones
-        if y > face_y+(face_h)*5/8 and y < face_y+face_h*5/6 and x > face_x+face_w*1/8 and x < face_x+face_w*7/8: 
-            color = "#AC8D59"
-            cv2.rectangle(frame, (x, y), (x + w, y + h), hex2bgr(color), 2)
-            cv2.putText(frame, 'smile', (x, y-10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.9, hex2bgr(color), 1)
+        smile = self.smile_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=7, minSize=(30, 20), maxSize=(80,40)) 
+        
+        face_x, face_y, face_w, face_h = 0,0,0,0
+        for (x, y, w, h) in faces:
+            face_x, face_y, face_w, face_h = x, y, w, h
+            if profile:
+                color = "#650EE8"
+                cv2.rectangle(frame, (x, y), (x + w, y + h), hex2bgr(color), 2)
+                cv2.putText(frame, 'profile', (x, y-10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.9, hex2bgr(color), 1)
+            else:
+                color = "#4E75E8"
+                cv2.rectangle(frame, (x, y), (x + w, y + h), hex2bgr(color), 2)
+                cv2.putText(frame, 'face', (x, y-10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.9, hex2bgr(color), 1)
             break
-    
-    end = time.time()
-    print(f"Tiempo de ejecución del pipeline: {(end - start):.4} segundos")
 
-    return frame
+        eyes_count = 0
+        for (x, y, w, h) in eyes:
+            if y > face_y and y < face_y+face_h*2/3 and x > face_x and x < face_x+face_w:
+                if eyes_count < 2:
+                    eyes_count += 1
+                else:
+                    break
+                color = "#C0E80E"
+                cv2.putText(frame, 'eye', (x, y-10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.9, hex2bgr(color), 1)
+                x = x + w//2
+                y = y + h//2
+                cv2.circle(frame, (x, y), w//2, hex2bgr(color), 2)
+                
+
+        for (x, y, w, h) in smile:
+            # Asegurar que la sonrisa esté dentro de la región segura de la cara y filtrar falsas detecciones
+            if y > face_y+(face_h)*5/8 and y < face_y+face_h*5/6 and x > face_x+face_w*1/8 and x < face_x+face_w*7/8: 
+                color = "#AC8D59"
+                cv2.rectangle(frame, (x, y), (x + w, y + h), hex2bgr(color), 2)
+                cv2.putText(frame, 'smile', (x, y-10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.9, hex2bgr(color), 1)
+                break
+        
+        end = time.time()
+        #print(f"Tiempo de ejecución del pipeline: {(end - start):.4} segundos")
+
+        return frame
+
 
 if __name__ == "__main__":
-    captura = captura_video(fps=30.0)
-    captura.record(save_video=False, pipe_filtrado=pipeFacesCV2)
+    captura = captura_video(fps=16.0)
+    pipelines = Pipelines_ImageProcessing()
+    captura.record(save_video=False, pipe_filtrado=pipelines.faceComponentsCV2)
